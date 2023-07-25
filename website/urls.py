be4f66e4ce2508b44fe6fd89087119bf9e1adc92
Thomas deBlaquiere <tdwebdesignmsu@yahoo.com>
2023-07-25 15:55:41 -0400

    added cfb assistant interface

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('process_question/', views.process_question, name='process_question'),
+    path('cfb_assistant/', views.cfb_assistant, name='cfb_assistant'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,4 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def process_question(request):
+    if request.method == 'POST':
+        question = request.POST.get('question')
+        response = question[::-1]  # Reverse the question
+        return HttpResponse(response)
+
+def cfb_assistant(request):
+    return render(request, 'cfb_assistant.html')
+
