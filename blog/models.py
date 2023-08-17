c917f1e741362677af97e34f005a8da179738a0d
Thomas deBlaquiere <tdwebdesignmsu@yahoo.com>
2023-08-17 13:41:07 -0400

    Models for Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Post(models.Model):
+    POST_STATUSES = [
+        ("draft", "Draft"),
+        ("published", "Published"),
+        ("archived", "Archived"),
+    ]
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+    content = RichTextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="posts",
+    )
+    post_status = models.CharField(
+        max_length=10, choices=POST_STATUSES, default="draft"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post-detail', kwargs={'slug': self.slug})
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="comments",
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    parent_comment = models.ForeignKey(
+        "self", on_delete=models.SET_NULL, null=True, blank=True, related_name="replies"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post.title}"
