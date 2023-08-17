2f8b3a189aab9ac4f5eb10401f05bf5fdd74b7c0
Thomas deBlaquiere <tdwebdesignmsu@yahoo.com>
2023-08-17 13:40:17 -0400

    Views and Templates for Blog

---
+++ blog/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from ckeditor.widgets import CKEditorWidget
+from .models import Post, Comment
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "slug", "content", "post_status"]
+        widgets = {
+            'slug': forms.TextInput(attrs={'readonly': 'readonly'}),
+        }
+
+    # Using CKEditor for the content field
+    content = forms.CharField(widget=CKEditorWidget())
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,102 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.utils.text import slugify
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post, Comment
+from .forms import CommentForm, PostForm  # You'll need to create these forms
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ["-created_at"]
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["comments"] = Comment.objects.filter(post=self.object)
+        context["form"] = CommentForm()
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = self.get_object()
+            comment.save()
+            return redirect('blog:post-detail', slug=self.get_object().slug)
+        return self.get(request, *args, **kwargs)
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.slug = slugify(form.instance.title)
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.slug = slugify(form.instance.title)
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("post-list")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = get_object_or_404(Post, slug=self.kwargs["slug"])
+        return super().form_valid(form)
+
+
+class ReplyCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    template_name = "blog/reply_form.html"
+    fields = ["content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = get_object_or_404(Post, slug=self.kwargs["slug"])
+        form.instance.parent_comment = get_object_or_404(
+            Comment, id=self.kwargs["comment_id"]
+        )
+        return super().form_valid(form)
