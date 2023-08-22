80394a43eb8b0135518292acbdc55cb844ca7ce1
Thomas deBlaquiere <tdwebdesignmsu@yahoo.com>
2023-08-22 11:01:29 -0400

    Fantasy News generation with chatGPT

---
+++ fantasy_news/services.py
@@ -0,0 +1,77 @@
+import requests
+import json
+import pandas as pd
+import os
+
+
+class FantasyLeague:
+    def __init__(self, league_id):
+        self.league_id = league_id
+        self.check_and_fetch_players()
+        self.players_df = self.load_players_from_file()
+        self.rosters_df = self.get_league_rosters()
+        self.player_id_to_name = dict(
+            zip(self.players_df["player_id"], self.players_df["full_name"])
+        )
+        self.player_name_to_id = dict(
+            zip(self.players_df["full_name"], self.players_df["player_id"])
+        )
+
+    def load_players_from_file(self, filename="fantasy_news/players.json"):
+        with open(filename, "r") as file:
+            players = json.load(file)
+        return pd.DataFrame(players).T
+
+    def get_league_rosters(self):
+        url = f"https://api.sleeper.app/v1/league/{self.league_id}/rosters"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return pd.DataFrame(response.json())
+        else:
+            print(
+                f"Error fetching rosters for league {self.league_id}: {response.status_code}"
+            )
+            return pd.DataFrame()
+
+    def get_roster_id_for_player(self, player_name):
+        player_id = self.player_name_to_id.get(player_name)
+        if not player_id:
+            print(f"Player ID not found for player: {player_name}")
+            return None
+        roster_row = self.rosters_df[
+            self.rosters_df["players"].apply(lambda x: player_id in x)
+        ]
+        return roster_row["owner_id"].values[0] if not roster_row.empty else None
+
+    def fetch_owner_info(self, owner_id):
+        url = f"https://api.sleeper.app/v1/user/{owner_id}"
+        response = requests.get(url)
+        return response.json() if response.status_code == 200 else None
+
+    def get_owner_for_player(self, player_name):
+        roster_id = self.get_roster_id_for_player(player_name)
+        if not roster_id:
+            return None
+        owner_info = self.fetch_owner_info(roster_id)
+        return owner_info["username"] if owner_info else None
+
+    @staticmethod
+    def fetch_players():
+        url = "https://api.sleeper.app/v1/players/nfl"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error fetching players: {response.status_code}")
+            return None
+
+    @staticmethod
+    def save_players_to_file(players):
+        with open("fantasy_news/players.json", "w") as file:
+            json.dump(players, file)
+
+    def check_and_fetch_players(self):
+        if not os.path.exists("fantasy_news/players.json"):
+            players = FantasyLeague.fetch_players()
+            if players:
+                FantasyLeague.save_players_to_file(players)
